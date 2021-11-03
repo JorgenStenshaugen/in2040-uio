6bdbe3977e307b6f1273d783561700425915a74b
Jørgen Stenshaugen <jorgenstenshaugen@gmail.com>
2021-11-03 18:47:39 +0100

    lint

--- oblig3a/oblig3a.scm
+++ oblig3a/oblig3a.scm
@@ -2,20 +2,20 @@
 
 " --- Oppgave 2a --- "
 (define (mem name proc)
-	;; Message passing for å kalle de ulike funksjonene basert på kallene.
-	(define (func)
-		(cond ((equal? name 'memoize) (memoize proc)) ;; Dersom man kaller på memoize
-			  ((equal? name 'unmemoize) (unmemoize proc)))) ;; Dersom man kaller på unmemoize
+  ;; Message passing for å kalle de ulike funksjonene basert på kallene.
+  (define (func)
+    (cond ((equal? name 'memoize) (memoize proc)) ;; Dersom man kaller på memoize
+          ((equal? name 'unmemoize) (unmemoize proc)))) ;; Dersom man kaller på unmemoize
 
-	;; EXPLAIN HERE
-	(define (memoize proc)
-		proc)
+  ;; EXPLAIN HERE
+  (define (memoize proc)
+    proc)
 
-	;; EXPLAIN HERE
-	(define (unmemoize proc)
-		proc)
+  ;; EXPLAIN HERE
+  (define (unmemoize proc)
+    proc)
 
-	func)
+  func)
 
 ;; Tests:
 (define fib (mem 'memoize fib))
