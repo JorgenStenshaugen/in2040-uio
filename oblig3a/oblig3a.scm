2bb94fe70cc685a9b48c272523d03ceea913744b
Yahya Isam <yahyai@uio.no>
2021-11-03 20:20:09 +0100

    Oppgave 1

--- oblig3a/oblig3a.scm
+++ oblig3a/oblig3a.scm
@@ -1,26 +1,49 @@
-(load "prekode3a.scm") ;; Last in prekode.
+(load "prekode3a.scm")
 
-" --- Oppgave 2a --- "
+;; --- Oppgave 2 ---
 (define (mem name proc)
   ;; Message passing for å kalle de ulike funksjonene basert på kallene.
-  (define (func)
+  (define (func name proc)
     (cond ((equal? name 'memoize) (memoize proc)) ;; Dersom man kaller på memoize
           ((equal? name 'unmemoize) (unmemoize proc)))) ;; Dersom man kaller på unmemoize
 
+  ;; --- Oppgave 2a ---
   ;; EXPLAIN HERE
   (define (memoize proc)
-    proc)
+    (let ((table (make-table)))
+      (lambda args
+        (if (null? args)
+            (proc args)
+        (or (lookup args table)
+            (let ((value (apply proc args)))
+              (insert! args value table)
+              value))))))
 
+  ;; --- Oppgave 2b ---
   ;; EXPLAIN HERE
   (define (unmemoize proc)
-    proc)
+    (define table (make-table))
+    (or (lookup proc table)
+       (proc)
+       ))
 
-  func)
+  (func name proc))
 
 ;; Tests:
-(define fib (mem 'memoize fib))
+" --- Oppgave 2a tests --- "
+(set! fib (mem 'memoize fib))
+(fib 3)
 (fib 3)
 (fib 2)
 (fib 4)
+" --- Oppgave 2b tests --- "
 (set! fib (mem 'unmemoize fib))
 (fib 3)
+
+" --- MORE TESTS --- "
+(set! test-proc (mem 'memoize test-proc))
+(test-proc)
+(test-proc)
+(test-proc 40 41 42 43 44)
+(test-proc 40 41 42 43 44)
+(test-proc 42 43 44)
