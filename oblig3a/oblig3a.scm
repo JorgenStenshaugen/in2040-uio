0d4be09a43636fbed5d0139fbc59b12e36214c5e
Jørgen Stenshaugen <jorgenstenshaugen@gmail.com>
2021-11-07 14:44:43 +0100

    Oblig 3a updates

--- oblig3a/oblig3a.scm
+++ oblig3a/oblig3a.scm
@@ -1,32 +1,33 @@
 (load "prekode3a.scm")
-
-;; --- Oppgave 2 ---
+(fib 3)
+(fib 2)
+(fib 1)
+(fib 3)
+;; --- Oppgave 1 ---
+(define procs (make-table))
 (define (mem name proc)
-  (let ((table (make-table)))
-  ;; Message passing for å kalle de ulike funksjonene basert på kallene.
+  ;; Message passing for å kalle på de ulike funksjonene basert på prosedyrene.
   (define (func name proc)
     (cond ((equal? name 'memoize) (memoize proc)) ;; Dersom man kaller på memoize
           ((equal? name 'unmemoize) (unmemoize proc)))) ;; Dersom man kaller på unmemoize
   
-  ;; --- Oppgave 2a ---
-  ;; EXPLAIN HERE
+  ;; --- Oppgave 1a ---
   (define (memoize proc)
-      (lambda args
-        (if (null? args)
-            proc
-        (or (lookup args table)
-            (let ((value (apply proc args)))
-              (insert! args value table)
-              value)))))
+    (let ((table (make-table)))
+      (let ((memoized 
+             (lambda args
+               (or (lookup args table)
+                   (let ((value (apply proc args)))
+                     (insert! args value table)
+                     value)))))
+      (insert! memoized proc procs)
+      memoized)))
 
-  ;; --- Oppgave 2b ---
-  ;; EXPLAIN HERE
+  ;; --- Oppgave 1b ---
   (define (unmemoize proc)
-    (or (lookup proc table)
-       (proc)
-       ))
+    (lookup proc procs))
 
-  (func name proc)))
+  (func name proc))
 
 ;; Tests:
 " --- Oppgave 2a tests --- "
@@ -46,3 +47,22 @@
 (test-proc 40 41 42 43 44)
 (test-proc 40 41 42 43 44)
 (test-proc 42 43 44)
+
+" --- 1 c --- "
+(define mem-fib (mem 'memoize fib))
+
+(mem-fib 3)
+(mem-fib 3)
+
+#|
+Hver gang man kaller på mem-fib så blir prosedyren mem kalt på nytt og kun det første kallet fra fib blir lagret i tabellen.
+I den memoiserte versjonen som settes med set! så kaller den alltid på den nye versjonen av fib, som altså har evaulerte verdier i tabellen sin.
+|#
+
+(define (list-to-stream liste)
+
+)
+
+(define (stream-to-list stream)
+
+)
