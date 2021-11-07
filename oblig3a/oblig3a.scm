362111bb25460a356ae9c90147143ea582718c9b
Yahya Isam <yahyai@uio.no>
2021-11-07 16:03:27 +0100

    Final drafts

--- oblig3a/oblig3a.scm
+++ oblig3a/oblig3a.scm
@@ -1,8 +1,5 @@
 (load "prekode3a.scm")
-(fib 3)
-(fib 2)
-(fib 1)
-(fib 3)
+
 ;; --- Oppgave 1 ---
 (define procs (make-table))
 (define (mem name proc)
@@ -29,18 +26,20 @@
 
   (func name proc))
 
-;; Tests:
-" --- Oppgave 2a tests --- "
+(newline)
+" --- Oppgave 1a tests --- "
 (set! fib (mem 'memoize fib))
 (fib 3)
 (fib 3)
 (fib 2)
 (fib 4)
-" --- Oppgave 2b tests --- "
+
+(newline)
+" --- Oppgave 1b tests --- "
 (set! fib (mem 'unmemoize fib))
 (fib 3)
 
-" --- MORE TESTS --- "
+" --- Flere tests --- "
 (set! test-proc (mem 'memoize test-proc))
 (test-proc)
 (test-proc)
@@ -48,21 +47,73 @@
 (test-proc 40 41 42 43 44)
 (test-proc 42 43 44)
 
-" --- 1 c --- "
+" --- Oppgave 1c tests --- "
 (define mem-fib (mem 'memoize fib))
 
 (mem-fib 3)
 (mem-fib 3)
+(mem-fib 2)
 
 #|
 Hver gang man kaller på mem-fib så blir prosedyren mem kalt på nytt og kun det første kallet fra fib blir lagret i tabellen.
 I den memoiserte versjonen som settes med set! så kaller den alltid på den nye versjonen av fib, som altså har evaulerte verdier i tabellen sin.
 |#
 
+;; --- Oppgave 2a ---
 (define (list-to-stream liste)
+  (if (null? liste)
+      the-empty-stream
+      (cons-stream (car liste) (list-to-stream (cdr liste)))))
 
-)
+(define (stream-to-list stream . args)
+  (if (stream-null? stream)
+      '()
+      (cond ((null? args)
+            (cons (stream-car stream) (stream-to-list (stream-cdr stream))))
+            ((= (car args) 0) '())
+            (else (cons (stream-car stream)
+                        (stream-to-list (stream-cdr stream) (- (car args) 1)))))))
 
-(define (stream-to-list stream)
+(newline)
+" --- Oppgave 2a tests --- "
+(list-to-stream '(1 2 3 4 5))
+(stream-to-list (stream-interval 10 20))
+(show-stream nats 15)
+(stream-to-list nats 10)
+(newline)
+(stream-car (integers-starting-from 10))
 
-)
+
+;; --- Oppgave 2b ---
+(define (stream-take n stream)
+  (if (or (stream-null? stream) (= n 0))
+      the-empty-stream
+      (cons-stream (stream-car stream)
+            (stream-take (- n 1) (stream-cdr stream)))))
+
+(newline)
+" --- Oppgave 2b tests --- "
+(define foo (stream-take 10 nats))
+foo
+(show-stream foo 5)
+(show-stream foo 20)
+(show-stream (stream-take 15 nats) 10)
+
+
+;; --- Oppgave 2c ---
+;; Et problem vi ser er at dersom vi har et uendelig strøm, vil prosedyrene gå inn i en uendelig løkke med kallet på stream-cdr.
+
+;; --- Oppgave 2d ---
+(define (remove-duplicates stream)
+  (if (stream-null? stream)
+      the-empty-stream
+      (cons-stream (stream-car stream)
+                   (remove-duplicates 
+                    (stream-filter
+                     (lambda (value) (not (equal? (stream-car stream) value)))
+                     (stream-cdr stream))))))
+
+(newline)
+" --- Oppgave 2d tests --- "
+(define test (list-to-stream '(2 2 3 4 5)))
+(show-stream (remove-duplicates test) 5)
